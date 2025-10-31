b8920897761e46790496ea3629d1a8166694b57a
Fuentes Leonel <lmfuentes00@gmail.com>
2025-10-31 13:43:04 -0300

    Elimina logs no necesarios

--- fp/backend/app/app.js
+++ fp/backend/app/app.js
@@ -5,53 +5,6 @@ const bodyParser = require('body-parser');
 require('../db/connection').getDB();
 require('./utils/initSuperAdmin').initSuperadmin();
 
-// --- simple runtime migration: ensure debts table has amount_paid column ---
-const { getDB } = require('../db/connection');
-const db = getDB();
-try {
-  const cols = db.prepare("PRAGMA table_info('debts')").all();
-  const hasAmountPaid = cols.some(c => c.name === 'amount_paid');
-  if (!hasAmountPaid) {
-    console.log('Adding amount_paid column to debts table');
-    db.exec("ALTER TABLE debts ADD COLUMN amount_paid NUMERIC NOT NULL DEFAULT 0;");
-  }
-} catch (err) {
-  console.warn('Could not run debts migration:', err.message);
-}
-
-// --- runtime migration: ensure organizations table has budget column ---
-try {
-  const colsOrg = db.prepare("PRAGMA table_info('organizations')").all();
-  const hasBudget = colsOrg.some(c => c.name === 'budget');
-  if (!hasBudget) {
-    console.log('Adding budget column to organizations table');
-    db.exec("ALTER TABLE organizations ADD COLUMN budget NUMERIC NOT NULL DEFAULT 0;");
-  }
-} catch (err) {
-  console.warn('Could not run organizations migration:', err.message);
-}
-
-// ensure payments.applied_to_budget exists
-try {
-  const colsPay = db.prepare("PRAGMA table_info('payments')").all();
-  const hasApplied = colsPay.some(c => c.name === 'applied_to_budget');
-  if (!hasApplied) {
-    console.log('Adding applied_to_budget column to payments table');
-    db.exec("ALTER TABLE payments ADD COLUMN applied_to_budget INTEGER NOT NULL DEFAULT 0;");
-  }
-} catch (err) {
-  console.warn('Could not run payments migration:', err.message);
-}
-
-// Recompute budgets from cash_movements log at startup
-try {
-  const CashMovementDAO = require('./dao/CashMovementDAO');
-  const res = CashMovementDAO.recomputeBudget();
-  if (res) console.log('Recomputed organization budget at startup:', res);
-} catch (e) {
-  console.warn('Could not recompute organization budget at startup:', e && e.message);
-}
-
 const userRoutes = require('./rest/UserRestController');
 const cashMovementRoutes = require("./rest/CashMovementRestController");
 const DebtRestController = require("./rest/DebtRestController");
